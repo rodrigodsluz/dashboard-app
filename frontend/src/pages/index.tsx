406148d0dfc3a522592569e9135a0e5201a0f320
rodrigodsluz <rodrigodsluz@gmail.com>
2021-01-28 10:59:06 -0300

    Fix

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,12 +1,11 @@
 import Head from 'next/head';
-import { Layout } from '../components';
 import { LineHeader } from '../components/LineHeader';
 import Login from './login';
 
 const Home: React.FC = () => (
   <>
     <Head>
-      <title>SuportApp</title>
+      <title>SupportApp</title>
     </Head>
 
     <LineHeader />
