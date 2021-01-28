32e0c8b551d56768a96cb0aa34bb95f8a9b94ac6
rodrigodsluz <rodrigodsluz@gmail.com>
2021-01-28 11:49:25 -0300

    Fix table container resposivity

--- frontend/src/pages/jvb/style.ts
+++ frontend/src/pages/jvb/style.ts
@@ -15,6 +15,18 @@ export const TableContainer = styled.div`
   display: flex;
   justify-content: space-evenly;
   flex-direction: column;
+
+  @media screen and (max-width: 929px) {
+    padding-top: 140px;
+  }
+
+  @media screen and (max-width: 645px) {
+    padding-top: 300px;
+  }
+
+  @media screen and (max-width: 540px) {
+    padding-top: 600px;
+  }
 `;
 
 export const CardContainer = styled.div`
