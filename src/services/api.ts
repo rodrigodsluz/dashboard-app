302565b36714bf862a65692a127108c35c80c865
rodrigodsluz <rodrigodsluz@gmail.com>
2021-02-25 17:57:30 -0300

    feat: add routes

--- src/services/api.ts
+++ src/services/api.ts
@@ -5,3 +5,103 @@ const api = axios.create({
 });
 
 export default api;
+
+export const getHomeData = async () => {
+  const token = localStorage.getItem('userToken');
+
+  try {
+    const { data } = await api.get('/Home/Summary', {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    return data.map(({
+      tenant, datamov, lote, produto, timer, sla, status,
+    }) => ({
+      tenant, datamov, lote, produto, timer, sla, status,
+    }));
+  } catch (error) {
+    return error;
+  }
+};
+
+export const getStoppedMovements = async () => {
+  const token = localStorage.getItem('userToken');
+
+  try {
+    const { data } = await api.get('/Home/StoppedMovements', {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    return data.map(({
+      tenant, datamov, lote, produto, timer, sla, status,
+    }) => ({
+      tenant, datamov, lote, produto, timer, sla, status,
+    }));
+  } catch (error) {
+    return error;
+  }
+};
+
+export const getPieChartSla = async () => {
+  const token = localStorage.getItem('userToken');
+
+  try {
+    const { data } = await api.get('/Home/PieChartSla', {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    return data.map(({
+      tenant, datamov, lote, produto, timer, sla, status,
+    }) => ({
+      tenant, datamov, lote, produto, timer, sla, status,
+    }));
+  } catch (error) {
+    return error;
+  }
+};
+
+export const getStoppedCount = async () => {
+  const token = localStorage.getItem('userToken');
+
+  try {
+    const { data } = await api.get('/Home/StoppedCount', {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    return data.map(({
+      tenant, datamov, lote, produto, timer, sla, status,
+    }) => ({
+      tenant, datamov, lote, produto, timer, sla, status,
+    }));
+  } catch (error) {
+    return error;
+  }
+};
+
+export const getBtnNotification = async () => {
+  const token = localStorage.getItem('userToken');
+
+  try {
+    const { data } = await api.get('/Home/BtnNotification', {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    return data.map(({
+      tenant, datamov, lote, produto, sla, status,
+    }) => ({
+      tenant, datamov, lote, produto, sla, status,
+    }));
+  } catch (error) {
+    return error;
+  }
+};
